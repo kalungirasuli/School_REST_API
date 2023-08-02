6bfa09f7951006c208549cb6a8f790de416183d7
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-08-03 00:04:03 +0300

    add post request for club

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const express= require('express');
+const express = require("express");
 const app = express();
-const port = process.env.PORT||3000;
-const router=express.Router();
+const port = process.env.PORT || 3000;
+const router = express.Router();
 const database = require("./database");
 const mongoose = require("mongoose");
 
-
 //middleware
-app.use(express.json());//the body of the request is json
-app.use(express.urlencoded({extended:true}));
-
+app.use(express.json()); //the body of the request is json
+app.use(express.urlencoded({ extended: true }));
 
 //connecting to the db
 mongoose.connect(database.connect, {
@@ -20,19 +18,15 @@ mongoose.connect(database.connect, {
 const db = mongoose.connection;
 //checking if db has connected
 db.once("open", () => {
-console.log("connected to db");
-})
+  console.log("connected to db");
+});
 db.on("error", (err) => {
-console.error(err);
+  console.error(err);
 });
 
-
-
-
-
-
-
-
+//import main route
+const mainRoute = require("./routes/mainRoute");
+app.use("/v1", mainRoute);
 
 //this is the listener of the server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- routes/clubRoute.js
+++ routes/clubRoute.js
@@ -1,24 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const clubModel = require("../models/club");
 
+router.post("/", async (req, res) => {
+  try {
+    const club = new clubModel(req.body);
+    await club.save();
+    res.status(200).send("Successfully added club");
+  } catch (error) {
+    res.status(500).send("Error occured");
+  }
+});
 
+router.get("/:{id}", async (req, res) => {});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports=router;
+module.exports = router;
