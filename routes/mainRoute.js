4da232399cefa00756c6154d6913673faa0ff5ea
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-08-03 11:54:14 +0300

    rename department resource to departments

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const clubRoute = require("./clubRoute");
-const studentRoute = require("../routes/studentRoutes")
-const courseRoute = require("../routes/courses")
-const staffRoute = require("../routes/staffRoute")
-const departmentRoute=require("../routes/deptRoutes")
+const studentRoute = require("../routes/studentRoutes");
+const courseRoute = require("../routes/courses");
+const staffRoute = require("../routes/staffRoute");
+const departmentRoute = require("../routes/deptRoutes");
 router.use("/clubs", clubRoute);
 router.use("/students", studentRoute);
-router.use("/courses",courseRoute)
-router.use("/staff",staffRoute)
-router.use("/department",departmentRoute)
+router.use("/courses", courseRoute);
+router.use("/staff", staffRoute);
+router.use("/departments", departmentRoute);
 
 module.exports = router;
