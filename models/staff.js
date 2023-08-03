7e0414c8a5f7418a67f067596a55c01dca8695ff
kalungirasuli <kalungirasuli495@gmail.com>
2023-08-03 10:33:26 +0300

    staff

---
+++ models/staff.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const staffSchema = new mongoose.Schema({
+    Id: {
+        type: String,
+        trim: true,
+    },
+    firstName: {
+        type: String,
+        trim: true,
+    },
+    secondName: {
+        type: String,
+        trim: true,
+    },
+    gender:{
+        type: String,
+        trim: true,
+    },
+    age:{
+        type: Number,
+        trim: true,
+    },
+    email:{
+        type: String,
+        trim: true,
+    },
+});
+
+module.exports = mongoose.model("staffModel", staffSchema);
