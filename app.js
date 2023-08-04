8ab73ffa7d16a00ec2c7953d3061b6450cce2f7e
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-08-04 10:12:11 +0300

    remove uncordinated code

--- app.js
+++ app.js
@@ -1,63 +1,27 @@
 const express = require("express");
 const app = express();
-const router=express.Router();
-const mongoose= require('mongoose')
-
-// const port = process.env.PORT || 3000;
-// const database = require("./database");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
 const database = require("./database");
-const mongoose = require("mongoose");
 
 //middleware
-app.use(express.json()); //the body of the request is json
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//connecting to the db
-// mongoose.connect(database.connect, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.connect(database.connect, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}); //connecting to the db
 
-// const db = mongoose.connection;
-// //checking if db has connected
-// db.once("open", () => {
-//   console.log("connected to db");
-// });
-// db.on("error", (err) => {
-//   console.error(err);
-// });
-
-// define a route handler for the default home page
-// const tryRoute=require('./routes/try.js');
-// const deptRoutes = require('./routes/deptRoutes.js')
-
-mongoose.connect('mongodb+srv://jumajosephat61:techfier@techfierdb.d6qutsb.mongodb.net/school_db', {
-    useNewUrlParser : true,
-    useUnifiedTopology : true
-})
-.then(()=>{
-    console.log('Connected to Database')
-})
-.catch((err)=>{
-    console.error('Error connecting to db')
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("connected to db");
+}); //checking if db has connected
+db.on("error", (err) => {
+  console.error(err);
 });
 
+const mainRoute = require("./routes/mainRoute"); //import main route
+app.use("/v1", mainRoute); //use route
 
-
-//this is the routes for trial
-// app.use('/',tryRoute)
-// app.use('/',deptRoutes)
-
-//this is the listener of the server
-app.listen(3000, () => console.log(`app listening on port 3000`));
-//import main route
-const mainRoute = require("./routes/mainRoute");
-
-//use route
-app.use("/v1", mainRoute);
-
-//this is the listener of the server
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- routes/deptRoutes.js
+++ routes/deptRoutes.js
@@ -3,23 +3,6 @@ const router = express.Router();
 const Department = require("../models/department");
 
 // POST API
-router.post('/', async (req, res) => {
-    try {
-      const { staff, departmentName, headOfDepartment, coursesInDepartment } = req.body;
-      const existingDepartment = await Department.findOne({ departmentName });
-      if (existingDepartment) {
-        return res.status(409).json({ error: 'there are no departments available' });
-      }
-      const newDepartment = new Department({
-        staff,
-        departmentName,
-        headOfDepartment,
-        coursesInDepartment
-      });
-      const savedDepartment = await newDepartment.save();
-      res.status(201).json(savedDepartment);
-    } catch (err) {
-      res.status(500).json({ error: 'Failed to create the department.' });
 router.post("/", async (req, res) => {
   try {
     const { staff, departmentName, headOfDepartment, coursesInDepartment } =
@@ -44,31 +27,6 @@ router.post("/", async (req, res) => {
 });
 
 // GET API FOR ALL DEPARTMENTS
-  router.get('/', async (req, res) => {
-    try {
-      const departments = await Department.find({});
-      res.status(200).json(departments);
-    } catch (err) {
-      res.status(500).json({ error: 'Failed to fetch departments.' });
-    }
-  });
-
-
-
-
-// GET API FOR A SPECIFIC DEPARTMENT
-  router.get('/:id', async (req, res) => {
-    try {
-      const departmentName = req.params.departmentName;
-      const department = await Department.findOne({ departmentName });
-  
-      if (!department) {
-        return res.status(404).json({ error: 'Department not found.' });
-      }
-  
-      res.status(200).json(department);
-    } catch (err) {
-      res.status(500).json({ error: 'Failed to fetch the department.' });
 router.get("/", async (req, res) => {
   try {
     const departments = await Department.find();
@@ -92,24 +50,6 @@ router.get("/:id", async (req, res) => {
 });
 
 // PUT API FOR A SPECIFIC DEPARTMENT
-  router.put('/:id', async (req, res) => {
-    try {
-      const departmentName = req.params.departmentName;
-      const updates = req.body;
-  
-      const updatedDepartment = await Department.findOneAndUpdate(
-        { departmentName },
-        updates,
-        { new: true }
-      );
-  
-      if (!updatedDepartment) {
-        return res.status(404).json({ error: 'Department not found.' });
-      }
-  
-      res.status(200).json(updatedDepartment);
-    } catch (err) {
-      res.status(500).json({ error: 'Failed to update the department.' });
 router.put("/:id", async (req, res) => {
   try {
     const updatedDepartment = await Department.findOneAndUpdate(
@@ -129,19 +69,6 @@ router.put("/:id", async (req, res) => {
 });
 
 // DELETE API FOR A SPECIFIC DEPARTMENT
-  router.delete('/:id', async (req, res) => {
-    try {
-      const departmentName = req.params.departmentName;
-  
-      const deletedDepartment = await Department.findOneAndDelete({ departmentName });
-  
-      if (!deletedDepartment) {
-        return res.status(404).json({ error: 'Department not found.' });
-      }
-  
-      res.status(200).json({ message: 'Department deleted successfully.' });
-    } catch (err) {
-      res.status(500).json({ error: 'Failed to delete the department.' });
 router.delete("/:id", async (req, res) => {
   try {
     const deletedDepartment = await Department.findOneAndDelete({
